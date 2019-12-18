9405a3608fe9823f9390a9912801a9a40b6abace
manjusa <thizismanju@gmail.com>
2019-12-18 14:34:06 +1100

    fixed remaining if updated budget

--- sampleUpdatedJSON.js
+++ sampleUpdatedJSON.js
@@ -1,35 +1,36 @@
 {
-"itemdetails":{
-  "furniture": [
-    {
-      "name": "harddisk",
-      "cost": "5000",
-      "boughtfrom": "kogan"
+    "itemdetails":{
+        "furniture": [
+            {
+                "name": "harddisk",
+                "cost": "5000",
+                "boughtfrom": "kogan"
+            },
+            {
+                "name": "teapoi",
+                "cost": "3000",
+                "boughtfrom": "kogan"
+            }
+        ],
+            "decors": [
+                {
+                    "name": "harddisk",
+                    "cost": "5000",
+                    "boughtfrom": "kogan"
+                },
+                {
+                    "name": "teapoi",
+                    "cost": "3000",
+                    "boughtfrom": "kogan"
+                }
+            ]
     },
-    {
-      "name": "teapoi",
-      "cost": "3000",
-      "boughtfrom": "kogan"
-    }
-  ],
-  "decors": [
-    {
-      "name": "harddisk",
-      "cost": "5000",
-      "boughtfrom": "kogan"
-    },
-    {
-      "name": "teapoi",
-      "cost": "3000",
-      "boughtfrom": "kogan"
-    }
-  ]
-  },
-  "items": 
-    {
-      "furniture": "1000",
-      "decors": "5000",
-      "remaining":"3000"
-    }
-  
+        "items": 
+        {
+            "furniture": "1000",
+                "decors": "5000",
+                    "remaining":"3000"
+        },
+    "total": "18000"
+
 }
